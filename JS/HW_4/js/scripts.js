b867de9afc1d12b944fa64bfdd73ed5cd57e9d8e
famillion <slapfama@gmail.com>
2020-07-12 17:22:18 +0300

    Home Work 4 completed

--- JS/HW_4/js/scripts.js
+++ JS/HW_4/js/scripts.js
@@ -175,34 +175,310 @@
 
 ////=================================================== 15 ==============================================================
 
-// - *** створити функцію яка буде переносити елементи з значенням 0 у кінець маисву. Зберігаючи при цьому порядок не нульових значень.
-// Двожина масиву від 2 до 100
-// Приклад
-// [1,0,6,0,3] => [1,6,3,0,0]
-// [0,1,2,3,4] => [1,2,3,4,0]
-// [0,0,1,0]   => [1,0,0,0]
-
-let testZeros = [0,0,1,0];
-
-console.log(testZeros);
-
-zeroToEndArr(testZeros);
-
-console.log(testZeros);
-
-function zeroToEndArr(array) {
-    let zeros = [];
-    for (let i = 0; i < array.length; i++) {
-        if(!array[i]){
-            let splice = array.splice(i, 1);
-            zeros.push(...splice);
-            i--;
-        }
-    }
-    if (zeros.length > 0) {
-        array.push(...zeros);
-    }
-}
+// // - *** створити функцію яка буде переносити елементи з значенням 0 у кінець маисву. Зберігаючи при цьому порядок не нульових значень.
+// // Двожина масиву від 2 до 100
+// // Приклад
+// // [1,0,6,0,3] => [1,6,3,0,0]
+// // [0,1,2,3,4] => [1,2,3,4,0]
+// // [0,0,1,0]   => [1,0,0,0]
+//
+// let testZeros = [0,0,1,0];
+//
+// console.log(testZeros);
+//
+// zeroToEndArr(testZeros);
+//
+// console.log(testZeros);
+//
+// function zeroToEndArr(array) {
+//     let zeros = [];
+//     for (let i = 0; i < array.length; i++) {
+//         if(!array[i]){
+//             let splice = array.splice(i, 1);
+//             zeros.push(...splice);
+//             i--;
+//         }
+//     }
+//     if (zeros.length > 0) {
+//         array.push(...zeros);
+//     }
+// }
 
 ////=================================================== 16 ==============================================================
 
+// // Створити функцію яка :
+// // - Додає в боді блок з текстом "Hello owu"
+//
+// function addHelloOwu() {
+//     let div = document.createElement('div');
+//     div.innerText = 'Hello owu';
+//     document.body.appendChild(div);
+// }
+//
+// addHelloOwu();
+
+////=================================================== 17 ==============================================================
+//
+// // - Додає в боді елемент з текстом . Тип елементу та текст отримати через аргументи
+//
+// function addElWithText(tag, text) {
+//     let el = document.createElement(tag);
+//     el.innerText = text;
+//     document.body.appendChild(el);
+// }
+//
+// ////=================================================== 18 ==============================================================
+//
+// // - приймає масив автомобілів (можна взяти з попередніх дз ),та  індентифікатор елемнту в який потрібно додати
+// // список цих автомобілів.
+// //     Для кожного автомобіля створити свій блок, та додати його в елемент, індентифікатор якого ви отримали.
+// //     Всі властивості авто в обному блоці
+//
+// let carsArr = [
+//     {brand: 'ZIL', engine: 'V1.5', color: 'blue', weight: 7, maxSpeed: 72},
+//     {brand: 'volvo', engine: 'V4', color: 'red', weight: 1.5, maxSpeed: 150},
+//     {brand: 'toyota', engine: 'V6', color: 'black', weight: 1.7, maxSpeed: 220},
+//     {brand: 'ZAZ', engine: 'V2', color: 'yellow', weight: 0.7, maxSpeed: 180},
+//     {brand: 'Belarus', engine: 'V6', color: 'blue', weight: 5, maxSpeed: 30}
+// ];
+//
+// function addCersToBlock(carsArr, tagWrap) {
+//     let wrap = document.createElement(tagWrap);
+//     for (const car of carsArr) {
+//         let carWrap = document.createElement('div');
+//         carWrap.style.padding = '5px';
+//         for (const field in car) {
+//             carWrap.innerHTML += `${field}: ${car[field]} <br>`;
+//         }
+//         wrap.appendChild(carWrap);
+//     }
+//     document.body.appendChild(wrap);
+// }
+//
+// addCersToBlock(carsArr, 'div');
+//
+// ////=================================================== 19 ==============================================================
+//
+// // - приймає масив автомобілів (можна взяти з попередніх дз ),та  індентифікатор елемнту в який потрібно
+// // додати список цих автомобілів.
+// //     Для кожного автомобіля створити свій блок, та додати його в елемент, індентифікатор якого ви отримали.
+// //     Для кожної властивості створити всередені блока автомоблія свій блок
+//
+// function addCersToBlocMod(carsArr, tagWrap) {
+//     let wrap = document.createElement(tagWrap);
+//     for (const car of carsArr) {
+//         let carWrap = document.createElement('div');
+//         carWrap.style.margin = '5px';
+//         carWrap.style.padding = '5px';
+//         carWrap.style.backgroundColor = 'aqua';
+//         for (const field in car) {
+//             let fieldWrap = document.createElement('div');
+//             fieldWrap.style.margin = '5px';
+//             fieldWrap.style.backgroundColor = 'pink';
+//             fieldWrap.innerHTML = `${field}: ${car[field]} <br>`;
+//             carWrap.appendChild(fieldWrap);
+//         }
+//         wrap.appendChild(carWrap);
+//     }
+//     document.body.appendChild(wrap);
+// }
+//
+// addCersToBlocMod(carsArr, 'div');
+
+////=================================================== 20 ==============================================================
+
+// // (на основі минулого ДЗ)
+// // **- функція приймає 2 масиви з рівною кількістю об'єктів та з'єднює в один об'єкт користувача та місто з відповідними "id" та "user_id",
+// // та повертає масив цих з'єднаних об'єктів.
+// //     Приклад масивів:
+// let usersWithId = [
+//     {id: 1, name: 'vasya', age: 31, status: false},
+//     {id: 2, name: 'petya', age: 30, status: true},
+//     {id: 3, name: 'kolya', age: 29, status: true},
+//     {id: 4, name: 'olya', age: 28, status: false}
+// ];
+// let citiesWithId = [
+//     {user_id: 3, country: 'USA', city: 'Portland'},
+//     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+//     {user_id: 2, country: 'Poland', city: 'Krakow'},
+//     {user_id: 4, country: 'USA', city: 'Miami'}
+// ];
+//
+// function mergeWithIdArr(userIdArr, citiesIdArr) {
+//     let merge = [];
+//     for (const user of userIdArr) {
+//         let obj = {...user};
+//         for (const city of citiesIdArr) {
+//             if (user.id === city.user_id) {
+//                 obj.address = city;
+//                 merge.push(obj);
+//             }
+//         }
+//     }
+//     return merge;
+// }
+//
+// let mergeArr = mergeWithIdArr(usersWithId, citiesWithId);
+// console.log(mergeArr);
+
+////=================================================== 21 ==============================================================
+
+// // ***- беремо завдання з правилами з укроку №3 :
+// // Та робимо це функцією.При цьому правила отримувати через аргумент.
+// // "Є масив котрий характеризує правила. Створити скрипт який ітерує цей масив, та робить з кожне правило в окремому блоці.
+// // При цому в блоці, номер правила записати в свій блок, текст правила записати в свій окремий блок.
+// //     Результатом відпрацювання скріпта повинна бути структура яка міститься в блоці wrap файла rule.html
+//
+// let rules = [
+//     {
+//         title: 'Первое правило Бойцовского клуба.',
+//         body: 'Никому не рассказывать о Бойцовском клубе.'
+//     },
+//     {
+//         title: 'Второе правило Бойцовского клуба.',
+//         body: 'Никогда никому не рассказывать о Бойцовском клубе.'
+//     },
+//     {
+//         title: 'Третье правило Бойцовского клуба.',
+//         body: 'В схватке участвуют только двое.'
+//     },
+//     {
+//         title: 'Четвертое правило Бойцовского клуба.',
+//         body: 'Не более одного поединка за один раз.'
+//     },
+//     {
+//         title: 'Пятое правило Бойцовского клуба.',
+//         body: 'Бойцы сражаются без обуви и голые по пояс.'
+//     },
+//     {
+//         title: 'Шестое правило Бойцовского клуба.',
+//         body: 'Поединок продолжается столько, сколько потребуется.'
+//     },
+//     {
+//         title: 'Седьмое правило Бойцовского клуба.',
+//         body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
+//     },
+//     {
+//         title: 'Восьмое и последнее правило Бойцовского клуба.',
+//         body: 'Новичок обязан принять бой.'
+//     },
+//
+// ];
+//
+// let ruleWrap = document.createElement('div');
+// ruleWrap.classList.add('wrap');
+//
+// function rulesToDiv(rule, wrapBlock) {
+//     let ruleDiv = document.createElement('div');
+//     let titleDiv = document.createElement('div');
+//     let bodyDiv = document.createElement('div');
+//
+//     ruleDiv.style.padding = '5px';
+//
+//     titleDiv.innerText = rule.title;
+//     ruleDiv.appendChild(titleDiv);
+//
+//     bodyDiv.innerText = rule.body;
+//     ruleDiv.appendChild(bodyDiv);
+//
+//     wrapBlock.appendChild(ruleDiv);
+// }
+//
+// for (const rule of rules) {
+//     rulesToDiv(rule, ruleWrap);
+// }
+//
+// document.body.appendChild(ruleWrap);
+
+////=================================================== 22 ==============================================================
+// // ===========додаткове від віктора========
+// // ---------------------------------------1) Точная степень двойки.-----------------------------------------------------
+// //     Дано натуральное число N.
+// //     Выведите слово YES, если число N является точной степенью двойки,
+// //     или слово NO в противном случае.
+// //     Операцией возведения в степень пользоваться нельзя!
+//
+// {
+//     function isExactPowerOfTwo(N) {
+//         while (N > 1) {
+//             if (N % 2 !== 0) {
+//                 return 'NO';
+//             }
+//             N /= 2;
+//         }
+//         return 'YES';
+//     }
+//
+//     let n = 4;
+//     console.log(isExactPowerOfTwo(n));
+// }
+//
+// //
+// //---------------------------------------- 2) Deep Copy ----------------------------------------------------------------
+// // реалізувати глибоке копіювання обєкту за допомогою рекурсій
+// //
+// {
+//     function objCopyRec(obj) {
+//         let newObj = {};
+//         for (const key in obj) {
+//             newObj[key] = (typeof obj[key] === 'object') ? objCopyRec(obj[key]) : obj[key];
+//         }
+//         return newObj;
+//     }
+//
+//     let usersTest = {
+//         name: 'vasya',
+//         age: 31,
+//         status: false,
+//         address: {
+//             city: 'Lviv',
+//             country: 'Ukraine',
+//             street: 'Shevchenko',
+//             houseNumber: {
+//                 city: {
+//                     city: 'New York',
+//                     country: 'USA',
+//                     street: 'East str',
+//                     houseNumber: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
+//                 },
+//                 country: 'USA',
+//                 street: 'East str',
+//                 houseNumber: 21
+//             }
+//         }
+//     };
+//
+//     let testObj = objCopyRec(usersTest);
+//
+//     console.log(testObj);
+//
+// }
+
+//
+// ------------------------------------------ 3) Flat ------------------------------------------------------------------
+// Вирівняти багаторівневий масив в однорівневий
+//     [1,3, ['Hello, 'Wordd', [9,6,1]], ['oops'], 9] -> [1, 3, 'Hello, 'Wordd', 9, 6, 1, 'oops', 9]
+// ===========додаткове========
+
+{
+    let arrTest = [1, 3, ['Hello', 'Wordd', [9,6,1]], ['oops'], 9];
+
+    // let arrFlat1 = arrTest.flat(2);
+    // console.log(arrFlat1);
+
+    let arrFlat2 = myFlat(arrTest);
+    console.log(arrFlat2);
+
+    function myFlat(arr) {
+        let newArr = [];
+        for (const el of arr) {
+            let newEl = (Array.isArray(el)) ? myFlat(el) : el;
+            if (Array.isArray(newEl)) {
+                newArr.push(...newEl);
+            } else {
+                newArr.push(newEl);
+            }
+        }
+        return newArr;
+    }
+}
