11733ffb66c7af904dbd67c97f105546d13ebfa8
famillion <slapfama@gmail.com>
2020-08-13 14:30:53 +0300

    MySQL less 2

--- SQL/HW_2/HW_2.sql
+++ SQL/HW_2/HW_2.sql
@@ -1,24 +1,4 @@
-### ============================================= SQL HOME WORK 2 ==========================================================
 
-#############   1. +Вибрати усіх клієнтів, чиє ім'я має менше ніж 6 символів.
-SELECT *
-FROM bank.client
-WHERE LENGTH(FirstName) < 6;
-
-############# 2. +Вибрати львівські відділення банку.+
-
-SELECT *
-FROM bank.department
-WHERE DepartmentCity = 'Lviv';
-
-############# 3. +Вибрати клієнтів з вищою освітою та посортувати по прізвищу.
-
-SELECT *
-FROM bank.client
-WHERE Education = 'high'
-ORDER BY LastName;
-
-############# 4. +Виконати сортування у зворотньому порядку над таблицею Заявка і вивести 5 останніх елементів.
 ### ============================================= SQL HOME WORK 2 ==========================================================
 
 #############   1. +Вибрати усіх клієнтів, чиє ім'я має менше ніж 6 символів.
@@ -110,8 +90,8 @@ GROUP BY ba.Client_idClient;
 
 ############# 12. Визначити найбільший та найменший кредити.
 
-SELECT MIN(ba.Sum) AS MinCredit,
-       MAX(ba.Sum) AS MaxCredit
+SELECT MIN(bank.application.Sum) AS MinCredit,
+       MAX(bank.application.Sum) AS MaxCredit
 FROM bank.application;
 
 ############# 13. Порахувати кількість кредитів для клієнтів,які мають вищу освіту.
@@ -159,8 +139,8 @@ SET ba.Sum      = 6000,
 WHERE bc.Education = 'high'
   AND ba.Client_idClient = bc.idClient;
 
-SELECT *
-FROM bank.application;
+# SELECT *
+# FROM bank.application;
 
 ############# 18. Усіх клієнтів київських відділень пересилити до Києва.
 
@@ -169,8 +149,8 @@ SET bc.City = 'Kyiv'
 WHERE bc.Department_idDepartment = bd.idDepartment
   AND bd.DepartmentCity = 'Kyiv';
 
-SELECT *
-FROM bank.client;
+# SELECT *
+# FROM bank.client;
 
 ############# 19. Видалити усі кредити, які є повернені.
 
@@ -178,8 +158,8 @@ DELETE
 FROM bank.application AS ba
 WHERE ba.CreditState = 'Returned';
 
-SELECT *
-FROM bank.application;
+# SELECT *
+# FROM bank.application;
 
 ############# 20. Видалити кредити клієнтів, в яких друга літера прізвища є голосною.
 
@@ -192,18 +172,96 @@ WHERE Client_idClient IN (SELECT DISTINCT bc.idClient
                                 WHERE REGEXP_LIKE(bc.LastName, '^.[aeiouy].*$')) AS bc);
 
 
-SELECT *
-FROM bank.application;
+# SELECT *
+# FROM bank.application;
 
 ############# 21. Знайти львівські відділення, які видали кредитів на загальну суму більше ніж 5000
 
+SELECT lvivDeps.idDepartment, lvivDeps.DepartmentCity, lvivDeps.allSum
+FROM (SELECT dep.idDepartment, dep.DepartmentCity, SUM(ba.Sum) AS allSum
+      FROM bank.department AS dep
+               JOIN bank.client bc on dep.idDepartment = bc.Department_idDepartment
+               JOIN application ba on bc.idClient = ba.Client_idClient
+      WHERE bc.Department_idDepartment IN (2, 5)
+      GROUP BY bc.Department_idDepartment) AS lvivDeps
+WHERE lvivDeps.allSum > 5000;
+
+############# 22. Знайти клієнтів, які повністю погасили кредити на суму більше ніж 5000
+
+SELECT res.idCl, clientFullName, allClientCredits
+FROM (SELECT bc.idClient                            AS idCl,
+             CONCAT(bc.FirstName, ' ', bc.LastName) AS clientFullName,
+             SUM(ba.Sum)                            AS allClientCredits
+      FROM bank.client bc
+               JOIN application ba on bc.idClient = ba.Client_idClient
+      WHERE ba.CreditState = 'Returned'
+      GROUP BY bc.idClient) AS res
+WHERE allClientCredits > 5000;
+
+############# 23. /* Знайти максимальний неповернений кредит.*/
+
+SELECT bc.idClient,
+       CONCAT(bc.FirstName, ' ', bc.LastName) AS fullName,
+       bc.Passport,
+       bc.City,
+       ba.Sum,
+       ba.CreditState
+FROM bank.application AS ba
+         JOIN bank.client bc on ba.Client_idClient = bc.idClient
+WHERE ba.CreditState = 'Not returned'
+ORDER BY ba.Sum DESC
+LIMIT 1;
+
+############# 24. /*Знайти клієнта, сума кредиту якого найменша*/
+
+SELECT bc.idClient,
+       CONCAT(bc.FirstName, ' ', bc.LastName) AS fullName,
+       bc.Passport,
+       bc.City,
+       SUM(ba.Sum)                            as allSum
+FROM bank.application AS ba
+         JOIN bank.client bc on ba.Client_idClient = bc.idClient
+GROUP BY bc.idClient
+ORDER BY allSum
+LIMIT 1;
+
+############# 25. /*Знайти кредити, сума яких більша за середнє значення усіх кредитів*/
+
+SELECT clients.idClient, clients.fullName, clients.Passport, clients.City, clients.allSum
+FROM (SELECT bc.idClient,
+             CONCAT(bc.FirstName, ' ', bc.LastName) AS fullName,
+             bc.Passport,
+             bc.City,
+             SUM(ba.Sum)                            as allSum
+      FROM bank.application AS ba
+               JOIN bank.client bc on ba.Client_idClient = bc.idClient
+      GROUP BY bc.idClient) AS clients
+WHERE clients.allSum > (SELECT AVG(Sum) FROM bank.application);
+
+############# 26. /*Знайти клієнтів, які є з того самого міста, що і клієнт, який взяв найбільшу кількість кредитів*/
+
+SELECT *
+FROM bank.client
+WHERE City = (SELECT cityCl.City
+              FROM (SELECT City, COUNT(Sum) as countSum
+                    FROM bank.client AS bc
+                             JOIN bank.application ba ON bc.idClient = ba.Client_idClient
+                    GROUP BY bc.idClient
+                    ORDER BY countSum DESC
+                    LIMIT 1) AS cityCl);
+
+############# 27. #місто чувака який набрав найбільше кредитів
+
+SELECT cityCl.City
+FROM (SELECT City, COUNT(Sum) as countSum
+      FROM bank.client AS bc
+               JOIN bank.application ba ON bc.idClient = ba.Client_idClient
+      GROUP BY bc.idClient
+      ORDER BY countSum DESC
+      LIMIT 1) AS cityCl
 
 
-
-
-
-
-
+########################################################################################################################
 
 
 
