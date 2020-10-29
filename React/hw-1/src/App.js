f49f601c074759e5bef82cc3b940f16f92659e9b
famillion <slapfama@gmail.com>
2020-10-29 22:50:49 +0200

    HW-1 react

---
+++ React/hw-1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Users from "./data/components/users-field/Users";
+import React from "react";
+
+
+function App() {
+  return (
+   <Users/>
+  );
+}
+
+export default App;
